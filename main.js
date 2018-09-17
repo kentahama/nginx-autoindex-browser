c28e582144ad7dda94416f04d3e0daf7fc7e581a
Kenta Hamanaka <kenta.hamanaka@gmail.com>
2018-09-17 10:33:12 +0900

    Update

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const JSON_API = "/json"
-const FILE_API = ""
+const FILE_API = "/private"
 const HOME = "/";
+var base = HOME;
+var parent = HOME;
 function init() {
     var param = new URLSearchParams(location.search);
     var path = param.get("p");
@@ -8,38 +10,42 @@ function init() {
 }
 function get(path) {
     var dirs = path.split("/");
-    var base = dirs.slice(0, -1).join("/") + "/";
-    var parent = dirs.slice(0, -2).join("/") + "/";
+    base = dirs.slice(0, -1).join("/") + "/";
+    parent = dirs.slice(0, -2).join("/") + "/";
     var wd_name = decodeURIComponent(dirs.slice(-2)[0]);
     if (path == "/") wd_name = "File browser";
     $("nav h1").text(wd_name);
     $.getJSON(JSON_API + path)
     .done(data => {
-	if (path != "/") add_item("parent", parent, "..");
-        var dirs = data.filter(d => d.type == "directory");
-	var files = data.filter(d => d.type == "file");
-        dirs.forEach(dir => {
-	    var path = base + encodeURIComponent(dir.name) + "/";
-	    add_item("directory", path, dir.name);
-	});	
-        files.forEach(file => {
-	    var path = base + encodeURIComponent(file.name);
-	    add_item("file", path, file.name);
-	});
+	if (path != "/")
+	    add_item({
+		type:"parent",
+		name: "Go to parent dir",
+	    });
+	data.forEach(add_item);
     }).fail((jqXHR, textStatus, errorThrown) => {
 	$("ul").append(`<p>An error occured: ${jqXHR.status} ${errorThrown}</p>`);
 	$("ul").append(`<p><a href='?p=${HOME}'>retrun to home</a></p>`);
     });
 }
-function add_item(type, path, text) {
+function add_item(item) {
     var icon = "";
-    switch (type) {
-    case "parent": icon = "fas fa-angle-left"; break;
-    case "directory": icon = "fas fa-folder"; break;
-    case "file": icon = "fas fa-file"; break;
+    var href = "";
+    switch (item.type) {
+    case "parent": icon = "fas fa-angle-left";
+	console.log(parent);
+	href = `?p=${parent}`;
+	break;
+    case "directory": icon = "fas fa-folder";
+	var path = base + encodeURIComponent(item.name) + "/";
+	href = `?p=${path}`;
+	break;
+    case "file": icon = "far fa-file";
+	href = FILE_API + base + encodeURIComponent(item.name);
+	break;
     }
-    var a = $("<a>", {href:`?p=${path}`})
-	.append($("<i>", {class: icon, style: "width: 16px;"}))
-	.append($("<div>", {class: "ml-2 d-inline", text:text}));
-    $("ul").append($("<li>").append(a));
+    var a = $("<a>", {href: href})
+	.append($("<i>", {class: icon}))
+	.append($("<div>", {class: "ml-2 d-inline", text:item.name}));
+    $("ul").append($("<li>", {class:item.type}).append(a));
 }
