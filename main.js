153ddaf0e4ea406fc41d33d06462a9d327bd4561
Kenta Hamanaka <kenta.hamanaka@gmail.com>
2018-09-21 01:30:24 +0900

    Add pretty bytes

--- main.js
+++ main.js
@@ -51,5 +51,46 @@ function add_item(item) {
 	.addClass(item.type)
 	.append($("<i>", {class: icon, style:"width:16px;"}))
 	.append($("<div>", {class: "ml-2 d-inline", text:item.name}));
+    if (item.size) a.append($("<div>", {class: "float-right", text:prettyBytes(item.size)}))
     $("main").append(a);
 }
+
+/*
+Formats the given number using `Number#toLocaleString`.
+- If locale is a string, the value is expected to be a locale-key (for example: `de`).
+- If locale is true, the system default locale is used for translation.
+- If no value for locale is specified, the number is returned unmodified.
+*/
+const UNITS = ['B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+const toLocaleString = (number, locale) => {
+    let result = number;
+    if (typeof locale === 'string') {
+	result = number.toLocaleString(locale);
+    } else if (locale === true) {
+	result = number.toLocaleString();
+    }
+    return result;
+};
+const prettyBytes = (number, options) => {
+    if (!Number.isFinite(number)) {
+	throw new TypeError(`Expected a finite number, got ${typeof number}: ${number}`);
+    }
+    options = Object.assign({}, options);
+    if (options.signed && number === 0) {
+	return ' 0 B';
+    }
+    const isNegative = number < 0;
+    const prefix = isNegative ? '-' : (options.signed ? '+' : '');
+    if (isNegative) {
+	number = -number;
+    }
+    if (number < 1) {
+	const numberString = toLocaleString(number, options.locale);
+	return prefix + numberString + ' B';
+    }
+    const exponent = Math.min(Math.floor(Math.log10(number) / 3), UNITS.length - 1);
+    number = Number((number / Math.pow(1000, exponent)).toPrecision(3));
+    const numberString = toLocaleString(number, options.locale);
+    const unit = UNITS[exponent];
+    return prefix + numberString + ' ' + unit;
+};
